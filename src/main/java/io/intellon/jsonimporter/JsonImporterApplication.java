2d19071fe88d46d0e0009271d572e7ab780a8c1a
Intellon <development@intellon.io>
2026-07-06 09:15:16 +0200

    Add Spring Boot 4.1 Maven scaffold

---
+++ src/main/java/io/intellon/jsonimporter/JsonImporterApplication.java
@@ -0,0 +1,12 @@
+package io.intellon.jsonimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonImporterApplication.class, args);
+    }
+}
